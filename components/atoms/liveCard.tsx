a5aba7b3f6e40801195420638a2b728202911c61
hato72 <hatotori72@gmail.com>
2024-03-11 18:59:36 +0900

    c

--- components/atoms/liveCard.tsx
+++ components/atoms/liveCard.tsx
@@ -72,7 +72,7 @@ const LiveCard = ({ isFixedVideo }: Props) => {
     ;(async () => {
       const res = await fetch(holoUrl, {
         headers: {
-          "X-APIKEY": process.env.NEXT_PUBLIC_API_KEY || "",
+          "X-APIKEY": process.env.NEXT_PUBLIC_API_KEY || "c910c475-f3b8-4980-b69a-4045e6aa10ff",
         },
       })
       const users = await res.json()
