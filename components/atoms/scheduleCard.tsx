101fe70f75fe7959d0031bdd8b052e19ad118600
hato72 <hatotori72@gmail.com>
2024-03-14 21:29:31 +0900

    live schedule対応

--- components/atoms/scheduleCard.tsx
+++ components/atoms/scheduleCard.tsx
@@ -1,8 +1,8 @@
 import dayjs from "dayjs"
-import { useEffect, useState } from "react"
+import { useEffect, useState,useContext } from "react"
 import { isCorrectLiveHoloUrl } from "../../utils/util"
 import { Api } from "./groupIcon"
-
+import { GroupContext } from "./groupContext"
 
 const ScheduleCard = () => {
   const youtube_jpeg = "https://img.youtube.com/vi/"
@@ -26,9 +26,139 @@ const ScheduleCard = () => {
     })()
   }, [])
 
+  const getFilteredData = (org: string) => {
+    return holoData.filter((data) => data.channel.org === org);
+  };
+  const {selectedGroup} = useContext(GroupContext)
+
   return (
     <div className="max-md:absolute flex justify-end mr-3 max-md:items-end max-md:flex-col right-[2px] top-[60px] z-[2]">
-      {holoData.map((holoDatas: Api) => {
+      {selectedGroup ===null && holoData.map((holoDatas: Api) => {
+        return isCorrectLiveHoloUrl(holoDatas) ? (
+          <>
+            <div className="relative w-[250px] max-xl:w-[24%] max-mm:w-[32.5%] max-md:w-[48.5%] max-sm:w-[48%.5] max-xs:w-[48.5%] h-full flex flex-col border shadow-sm rounded-xl bg-[#223e] border-gray-700 shadow-slate-700/[.7]">
+              <div className="absolute text-xs font-bold text-center text-gray-200 bottom-1 right-2 opacity-90 max-sm:text-[10px]">
+                <span className="mr-[1px]">◎</span>配信予定
+              </div>
+              <a href={`${holoVideo}${holoDatas.id}`} target="_blank">
+                <div className="">
+                    <img className="w-full h-auto rouded-t-xl" src={youtube_jpeg + holoDatas.id + youtube_jpeg_size.large} alt="Image Description"/>
+                    <div className="p-3 md:p-4">
+                        <div className="text-gray-400 max-sm:text-[14px]">
+                            {dayjs(holoDatas.start_scheduled).format("MM-DD HH:mm")}
+                        </div>
+                        <h3 className="flex font-bold text-md text-white max-sm:text-[12px]">
+                            {holoDatas.title}
+                        </h3>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </>
+        ) : null
+      })}
+
+      {selectedGroup==="All Group" && holoData.map((holoDatas: Api) => {
+        return isCorrectLiveHoloUrl(holoDatas) ? (
+          <>
+            <div className="relative w-[250px] max-xl:w-[24%] max-mm:w-[32.5%] max-md:w-[48.5%] max-sm:w-[48%.5] max-xs:w-[48.5%] h-full flex flex-col border shadow-sm rounded-xl bg-[#223e] border-gray-700 shadow-slate-700/[.7]">
+              <div className="absolute text-xs font-bold text-center text-gray-200 bottom-1 right-2 opacity-90 max-sm:text-[10px]">
+                <span className="mr-[1px]">◎</span>配信予定
+              </div>
+              <a href={`${holoVideo}${holoDatas.id}`} target="_blank">
+                <div className="">
+                    <img className="w-full h-auto rouded-t-xl" src={youtube_jpeg + holoDatas.id + youtube_jpeg_size.large} alt="Image Description"/>
+                    <div className="p-3 md:p-4">
+                        <div className="text-gray-400 max-sm:text-[14px]">
+                            {dayjs(holoDatas.start_scheduled).format("MM-DD HH:mm")}
+                        </div>
+                        <h3 className="flex font-bold text-md text-white max-sm:text-[12px]">
+                            {holoDatas.title}
+                        </h3>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </>
+        ) : null
+      })}
+
+      {selectedGroup==="Hololive" && getFilteredData("Hololive").map((holoDatas: Api) => {
+        return isCorrectLiveHoloUrl(holoDatas) ? (
+          <>
+            <div className="relative w-[250px] max-xl:w-[24%] max-mm:w-[32.5%] max-md:w-[48.5%] max-sm:w-[48%.5] max-xs:w-[48.5%] h-full flex flex-col border shadow-sm rounded-xl bg-[#223e] border-gray-700 shadow-slate-700/[.7]">
+              <div className="absolute text-xs font-bold text-center text-gray-200 bottom-1 right-2 opacity-90 max-sm:text-[10px]">
+                <span className="mr-[1px]">◎</span>配信予定
+              </div>
+              <a href={`${holoVideo}${holoDatas.id}`} target="_blank">
+                <div className="">
+                    <img className="w-full h-auto rouded-t-xl" src={youtube_jpeg + holoDatas.id + youtube_jpeg_size.large} alt="Image Description"/>
+                    <div className="p-3 md:p-4">
+                        <div className="text-gray-400 max-sm:text-[14px]">
+                            {dayjs(holoDatas.start_scheduled).format("MM-DD HH:mm")}
+                        </div>
+                        <h3 className="flex font-bold text-md text-white max-sm:text-[12px]">
+                            {holoDatas.title}
+                        </h3>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </>
+        ) : null
+      })}
+
+      {selectedGroup==="Njisanji" && getFilteredData("Njisanji").map((holoDatas: Api) => {
+        return isCorrectLiveHoloUrl(holoDatas) ? (
+          <>
+            <div className="relative w-[250px] max-xl:w-[24%] max-mm:w-[32.5%] max-md:w-[48.5%] max-sm:w-[48%.5] max-xs:w-[48.5%] h-full flex flex-col border shadow-sm rounded-xl bg-[#223e] border-gray-700 shadow-slate-700/[.7]">
+              <div className="absolute text-xs font-bold text-center text-gray-200 bottom-1 right-2 opacity-90 max-sm:text-[10px]">
+                <span className="mr-[1px]">◎</span>配信予定
+              </div>
+              <a href={`${holoVideo}${holoDatas.id}`} target="_blank">
+                <div className="">
+                    <img className="w-full h-auto rouded-t-xl" src={youtube_jpeg + holoDatas.id + youtube_jpeg_size.large} alt="Image Description"/>
+                    <div className="p-3 md:p-4">
+                        <div className="text-gray-400 max-sm:text-[14px]">
+                            {dayjs(holoDatas.start_scheduled).format("MM-DD HH:mm")}
+                        </div>
+                        <h3 className="flex font-bold text-md text-white max-sm:text-[12px]">
+                            {holoDatas.title}
+                        </h3>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </>
+        ) : null
+      })}
+
+      {selectedGroup==="Aogiri Highschool" && getFilteredData("Aogiri Highschool").map((holoDatas: Api) => {
+        return isCorrectLiveHoloUrl(holoDatas) ? (
+          <>
+            <div className="relative w-[250px] max-xl:w-[24%] max-mm:w-[32.5%] max-md:w-[48.5%] max-sm:w-[48%.5] max-xs:w-[48.5%] h-full flex flex-col border shadow-sm rounded-xl bg-[#223e] border-gray-700 shadow-slate-700/[.7]">
+              <div className="absolute text-xs font-bold text-center text-gray-200 bottom-1 right-2 opacity-90 max-sm:text-[10px]">
+                <span className="mr-[1px]">◎</span>配信予定
+              </div>
+              <a href={`${holoVideo}${holoDatas.id}`} target="_blank">
+                <div className="">
+                    <img className="w-full h-auto rouded-t-xl" src={youtube_jpeg + holoDatas.id + youtube_jpeg_size.large} alt="Image Description"/>
+                    <div className="p-3 md:p-4">
+                        <div className="text-gray-400 max-sm:text-[14px]">
+                            {dayjs(holoDatas.start_scheduled).format("MM-DD HH:mm")}
+                        </div>
+                        <h3 className="flex font-bold text-md text-white max-sm:text-[12px]">
+                            {holoDatas.title}
+                        </h3>
+                  </div>
+                </div>
+              </a>
+            </div>
+          </>
+        ) : null
+      })}
+
+      {selectedGroup==="VSpo" && getFilteredData("VSpo").map((holoDatas: Api) => {
         return isCorrectLiveHoloUrl(holoDatas) ? (
           <>
             <div className="relative w-[250px] max-xl:w-[24%] max-mm:w-[32.5%] max-md:w-[48.5%] max-sm:w-[48%.5] max-xs:w-[48.5%] h-full flex flex-col border shadow-sm rounded-xl bg-[#223e] border-gray-700 shadow-slate-700/[.7]">
