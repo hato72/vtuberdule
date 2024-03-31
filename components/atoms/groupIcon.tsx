fb8c021e3d1ecacbd21abae6e789f2727b4c264d
hato72 <hatotori72@gmail.com>
2024-03-31 19:43:41 +0900

    key

--- components/atoms/groupIcon.tsx
+++ components/atoms/groupIcon.tsx
@@ -42,9 +42,9 @@ const GroupIcon = () => {
     ;(async () => {
       const res = await fetch(holoUrl, {
         headers: {
-          //"X-APIKEY": process.env.HOLODEX_API_KEY || ""
+          "X-APIKEY": process.env.NEXT_PUBLIC_HOLODEX_API_KEY || ""
           //"X-APIKEY": HOLODEX_API_KEY
-          "X-APIKEY": "c910c475-f3b8-4980-b69a-4045e6aa10ff"
+          //"X-APIKEY": "c910c475-f3b8-4980-b69a-4045e6aa10ff"
         },
       })
       const users = await res.json()
--- components/atoms/scheduleCard.tsx
+++ components/atoms/scheduleCard.tsx
@@ -31,8 +31,8 @@ const ScheduleCard = ({searchQuery}:Props) => {
     ;(async () => {
       const res = await fetch(holoUrl, {
         headers: {
-          //"X-APIKEY": process.env.HOLODEX_API_KEY || "",
-          "X-APIKEY": process.env.HOLODEX_API_KEY || "c910c475-f3b8-4980-b69a-4045e6aa10ff"
+          "X-APIKEY": process.env.NEXT_PUBLIC_HOLODEX_API_KEY || "",
+          //"X-APIKEY": process.env.HOLODEX_API_KEY || "c910c475-f3b8-4980-b69a-4045e6aa10ff"
           //"X-APIKEY": HOLODEX_API_KEY
         },
       })
